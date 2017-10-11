06ddd10958a0ea5329fc61ecb49c0fbea071cdde
Rishi Bedi <rbedi100@gmail.com>
2017-10-11 06:32:02 +0000

    Runner scripts

---
+++ runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FASTA_FILE=${1}
+RESULTS_DIR=${2}
+
+mkdir -p ${RESULTS_DIR}
+cp ${FASTA_FILE} ${RESULTS_DIR}/input_seqs.fa
+cd /data/projects/rishi/vdjones
+source ./env/bin/activate
+python vdjones.py ${RESULTS_DIR}/input_seqs.fa ${RESULTS_DIR}
+
